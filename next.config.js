e205006eef00c0a8a4cd69e071007e9960bd76ee
DoLe-DangKhoa <doledangkhoa2@gmail.com>
2023-05-27 22:58:48 +0700

    lan 2

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     env: {
-        "BASE_URL": "http://localhost",
+        "BASE_URL": "https://khoaluan.onrender.com",
         "MONGODB_URL": "mongodb+srv://root:sapassword@cluster0.zo3mqh7.mongodb.net/?retryWrites=true&w=majority",
         "ACCESS_TOKEN_SECRET": ".q~C<9G4jYzTZCBT!y,[V?3x,:.2'CD!J`S+?7kAm4SY%3{R>a",
         "REFRESH_TOKEN_SECRET": "QEQr]crS.h,[MERm2Z'`}!(YrTQ^>Yt!xCcL#xc2WFgk?W4~b,UzWtAHe8[s<a5xjcn5US+M'7_b",
@@ -10,8 +10,9 @@ module.exports = {
         "CLOUD_API": "https://api.cloudinary.com/v1_1/dzygop1hq/image/upload",
         "GOOGLE_CLIENT_ID":"862778676575-e7jnu2hgfcjduhjtl8ka1vrg5qkshkpu.apps.googleusercontent.com",
         "GOOGLE_CLIENT_SECRET":"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",
-        "NEXTAUTH_URL":"http://localhost",
-        "PORT_ENV": 3000
+       
+        
 
-    }
+    },
+    distDir:"build"
 }
--- utils/fetchData.js
+++ utils/fetchData.js
@@ -1,8 +1,8 @@
 const baseUrl = process.env.BASE_URL
-const PORT = process.env.PORT || process.env.PORT_ENV
+
 
 export const getData = async (url, token) => {
-    const res = await fetch(`${baseUrl}:${PORT}/api/${url}`, {
+    const res = await fetch(`${baseUrl}/api/${url}`, {
         method: 'GET',
         headers: {
             'Authorization': token
@@ -14,7 +14,7 @@ export const getData = async (url, token) => {
 }
 
 export const postData = async (url, post, token) => {
-    const res = await fetch(`${baseUrl}:${PORT}/api/${url}`, {
+    const res = await fetch(`${baseUrl}/api/${url}`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -30,7 +30,7 @@ export const postData = async (url, post, token) => {
 
 
 export const putData = async (url, post, token) => {
-    const res = await fetch(`${baseUrl}:${PORT}/api/${url}`, {
+    const res = await fetch(`${baseUrl}/api/${url}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
@@ -44,7 +44,7 @@ export const putData = async (url, post, token) => {
 }
 
 export const patchData = async (url, post, token) => {
-    const res = await fetch(`${baseUrl}:${PORT}/api/${url}`, {
+    const res = await fetch(`${baseUrl}/api/${url}`, {
         method: 'PATCH',
         headers: {
             'Content-Type': 'application/json',
@@ -59,7 +59,7 @@ export const patchData = async (url, post, token) => {
 
 
 export const deleteData = async (url, token) => {
-    const res = await fetch(`${baseUrl}:${PORT}/api/${url}`, {
+    const res = await fetch(`${baseUrl}/api/${url}`, {
         method: 'DELETE',
         headers: {
             'Content-Type': 'application/json',
